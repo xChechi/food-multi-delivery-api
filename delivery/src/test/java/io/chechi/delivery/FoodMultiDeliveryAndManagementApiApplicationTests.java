a6564f613e71bbd370a72339fefb08c5f73446e9
xChechi <stefannasev81@gmail.com>
2023-11-30 20:46:16 +0000

    1st commit

---
+++ delivery/src/test/java/io/chechi/delivery/FoodMultiDeliveryAndManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chechi.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMultiDeliveryAndManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
